9a0eb69e43e913c4aae34df3552c0268fb2e56e2
Azareal2120 <36973779+Azareal2120@users.noreply.github.com>
2022-06-17 08:29:42 +1000

    Create expressions.c

---
+++ tut02/expressions.c
@@ -0,0 +1,20 @@
+// Sample code to process expressions
+// Written by Riley Dean, z5308666
+
+#include <stdio.h>
+
+int main(void) {
+    int expression1 = 7 / 2;
+    printf("%d\n", expression1);
+    
+    double expression2 = (3.0 / 2) + 1;
+    printf("%.2lf\n", expression2);
+    
+    char expression3 = 'a' + 5;
+    printf("%c\n", expression3);
+    
+    char expression4 = 'F' - 'A' + 'a';
+    printf("%c\n", expression4);
+    
+    return 0;
+}
